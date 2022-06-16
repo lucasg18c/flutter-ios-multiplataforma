e72a9da0d274f347b5ed1bf458247acced619dcf
lucasg18c <lucasg18c@gmail.com>
2022-06-16 09:42:42 -0300

    Auth

--- lib/auth/authManager.dart
+++ lib/auth/authManager.dart
@@ -1,7 +1,11 @@
+import 'dart:developer';
+
 import 'package:local_auth/local_auth.dart';
 
 import '../web/WebControl.dart';
 import '../web/WebHandler.dart';
+import 'package:local_auth_android/local_auth_android.dart';
+import 'package:local_auth_ios/local_auth_ios.dart';
 
 class AuthManager {
   final LocalAuthentication auth = LocalAuthentication();
@@ -13,7 +17,31 @@ class AuthManager {
 
   dynamic autenticar(List<dynamic> params) async {
     String motivo = params[0]["motivo"];
-    bool autenticado = await auth.authenticate(localizedReason: motivo);
+    log("Puede ver biometrics: ${await auth.canCheckBiometrics}");
+    log("Biometrics disponibles: ${await auth.getAvailableBiometrics()}");
+
+    bool autenticado = await auth.authenticate(
+        localizedReason: motivo,
+        authMessages: [
+          const AndroidAuthMessages(
+              biometricHint: "Te doy una pista",
+              biometricNotRecognized: "Biometrics no reconocido",
+              biometricRequiredTitle: "Uh, validate con biometrics",
+              biometricSuccess: "Éxito!",
+              cancelButton: "Nah dejá",
+              deviceCredentialsRequiredTitle: "Credenciales requeridas",
+              deviceCredentialsSetupDescription: "Preparar credenciales",
+              goToSettingsButton: "Ir a ajustes",
+              goToSettingsDescription: "Descripción, ir a ajustes",
+              signInTitle: "A U T E N T I C A T E"),
+          const IOSAuthMessages(
+              lockOut: "Lock out?",
+              goToSettingsDescription: "Descripcion, Ir a ajustes",
+              goToSettingsButton: "Ir a ajustes",
+              cancelButton: "Cancelar",
+              localizedFallbackTitle: "Eee fallback")
+        ],
+        options: const AuthenticationOptions(biometricOnly: true));
     return {"autenticado": autenticado};
   }
 
